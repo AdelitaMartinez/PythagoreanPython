aaf90c4e84aac5b6ddc9abb7ba0b2f9f362609cd
Adelita Martinez <amartinez1013@cnm.edu>
2024-05-16 08:29:42 -0600

    Adding psudocode

--- pythagSide.py
+++ pythagSide.py
@@ -2,3 +2,15 @@
 # Programmer: Adelita Martinez (amartinez1013@gmail.com)
 # date: 16 May 2024
 # Purpose: Find the length of the side of a right triangle
+# using the pythagorean formuola
+
+# Display a header explaining the program 
+
+# Get lenght of one size from the user
+
+# Get length of the hypotenuse (opposite side from right angle)
+
+# Calculate the length of the side
+
+# a^2 + b^2 = c^2
+# 
